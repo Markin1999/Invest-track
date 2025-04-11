887823e01898001366cc7d8096876a4ff76bf00a
Marco Filannino <filanninobusiness@gmail.com>
2025-04-11 02:09:53 +0200

    Sistemato css navbar

--- vite-project/src/componenti/Navbar.jsx
+++ vite-project/src/componenti/Navbar.jsx
@@ -6,24 +6,62 @@ export function Navbar() {
   const { user } = useUserContext();
 
   return (
-    <div>
-      <div>Invest-Track</div>
-
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>Investimenti</li>
-          <li>Statistiche</li>
-          <li>Diario</li>
-          <li>Notizie</li>
+    <nav className="flex justify-between items-center bg-[#1a73e8] px-8 py-4 text-white">
+      <div className="flex gap-8">
+        <div>
+          <h1 className="text-[1.8rem] font-semibold">Invest-Track</h1>
+        </div>
+        <ul className="flex items-center gap-6 list-none">
+          <li>
+            {" "}
+            <a
+              className=" text-white font-medium transition-colors duration-200 ease-in-out hover:text-[#cce2ff]"
+              href=""
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              className=" text-white font-medium transition-colors duration-200 ease-in-out hover:text-[#cce2ff]"
+              href=""
+            >
+              Investimenti
+            </a>
+          </li>
+          <li>
+            <a
+              className=" text-white font-medium transition-colors duration-200 ease-in-out hover:text-[#cce2ff]"
+              href=""
+            >
+              Statistiche
+            </a>
+          </li>
+          <li>
+            <a
+              className=" text-white font-medium transition-colors duration-200 ease-in-out hover:text-[#cce2ff]"
+              href=""
+            >
+              Diario
+            </a>
+          </li>
+          <li>
+            <a
+              className=" text-white font-medium transition-colors duration-200 ease-in-out hover:text-[#cce2ff]"
+              href=""
+            >
+              Notizie
+            </a>
+          </li>
         </ul>
       </div>
 
       <div>
         <p>
-          Benvenuto, <strong>{user.nome}</strong>
+          Benvenuto, <strong>{user.nome}</strong>{" "}
+          <strong>{user.cognome}</strong>
         </p>
       </div>
-    </div>
+    </nav>
   );
 }
